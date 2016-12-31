bc3410396280e30bc3734a5d7f678b3c9d437661
erikvw <ew2789@gmail.com>
2016-12-31 17:12:33 +0200

    change search to icon, remove incorrect helper attrs, replace search_url_name class attr with list_url

--- edc_search/forms.py
+++ edc_search/forms.py
@@ -8,17 +8,17 @@ from crispy_forms.layout import Layout
 class SearchForm(forms.Form):
 
     search_term = forms.CharField(
-        label='',
+        label='Search',
         max_length=36,
         required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper = FormHelper()
-        self.helper.form_action = None
+        self.helper.form_action = None  # set in child class
         self.helper.form_id = 'form-search'
         self.helper.form_method = 'post'
-        # self.helper.html5_required = True
+        self.helper.form_show_labels = False
+        self.helper.html5_required = False
         self.helper.layout = Layout(
-            FieldWithButtons('search_term', StrictButton('Search', type='submit')))
+            FieldWithButtons('search_term', StrictButton('<i class="fa fa-search fa-sm"></i>', type='submit')))
--- edc_search/view_mixins.py
+++ edc_search/view_mixins.py
@@ -9,8 +9,13 @@ class SearchViewMixin:
     form_class = SearchForm
     template_name = None
     paginate_by = 10
-    search_url_name = None
     search_model = None
+    url_lookup_parameters = []
+    list_url = '/'
+
+    def __init__(self, **kwargs):
+        self.filter_options = {}
+        super().__init__(**kwargs)
 
     def search_options(self, search_term, **kwargs):
         """Override to return options for the search model filter().
@@ -57,5 +62,34 @@ class SearchViewMixin:
             else:
                 results = self.paginate(self.search_model.objects.all().order_by('-created'))
             context = self.get_context_data()
-            context.update(form=form, results=results)
+            context.update(form=form, results=results, search_term=search_term)
         return self.render_to_response(context)
+
+    @property
+    def filtered_results(self):
+        """Returns a queryset filtered by URL parameters from the context."""
+        return self.search_model.objects.filter(**self.filter_options).order_by('-created')
+
+    def update_filter_options_from(self, context):
+        """Returns options for the filter() of `filtered_results`."""
+        lookups = []
+        for attrname in self.url_lookup_parameters:
+            if isinstance(attrname, tuple):
+                lookups.append((attrname[0], attrname[1]))
+            else:
+                lookups.append((attrname, attrname))
+        for attrname, lookup in lookups:
+            if context.get(attrname):
+                self.filter_options = {lookup: context.get(attrname)}
+                context['search_term'] = context.get(attrname)
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = self.update_filter_options_from(context)
+        context.update(
+            show_paginator_control=True if self.filtered_results.count() > self.paginate_by else False,
+            list_url=self.list_url,
+            results=self.paginate(self.filtered_results),
+            result_count_total=self.filtered_results.count())
+        return context
