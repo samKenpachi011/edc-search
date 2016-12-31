f94227eb9e7ad6539d74a5d136a54f69dae788c9
erikvw <ew2789@gmail.com>
2016-12-31 02:46:49 +0200

    fix to return queryset if no search term

--- edc_search/view_mixins.py
+++ edc_search/view_mixins.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import MultipleObjectsReturned
+from django.core.paginator import Paginator, EmptyPage
 
 from edc_search.forms import SearchForm
 
@@ -12,6 +13,9 @@ class SearchViewMixin:
     search_model = None
 
     def search_options(self, search_term, **kwargs):
+        """Override to return options for the search model filter().
+
+        Returns a Q object and additional kwargs for the filter"""
         q = ()
         options = {}
         return q, options
@@ -20,6 +24,7 @@ class SearchViewMixin:
         return qs
 
     def queryset(self, search_term, **kwargs):
+        """Returns a queryset matching the search term."""
         q, options = self.search_options(search_term, **kwargs)
         try:
             qs = [self.search_model.objects.get(q, **options)]
@@ -27,7 +32,17 @@ class SearchViewMixin:
             qs = None
         except MultipleObjectsReturned:
             qs = self.search_model.objects.filter(q, **options).order_by('-created')
-        return self.queryset_wrapper(qs)
+        return qs
+
+    def paginate(self, qs):
+        """Paginates a queryset."""
+        paginator = Paginator(qs, self.paginate_by)
+        paginator.object_list = self.queryset_wrapper(paginator.object_list)
+        try:
+            page = paginator.page(self.kwargs.get('page', 1))
+        except EmptyPage:
+            page = paginator.page(paginator.num_pages)
+        return page
 
     def form_valid(self, form):
         if form.is_valid():
@@ -39,6 +54,8 @@ class SearchViewMixin:
                     form.add_error('search_term', 'No matching records for \'{}\'.'.format(search_term))
                 else:
                     results = self.paginate(qs)
+            else:
+                results = self.paginate(self.search_model.objects.all().order_by('-created'))
             context = self.get_context_data()
             context.update(form=form, results=results)
         return self.render_to_response(context)
