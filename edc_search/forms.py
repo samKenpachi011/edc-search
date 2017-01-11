24d4c7384d7fe861a58b7572aca297bea8cd8656
erikvw <ew2789@gmail.com>
2017-01-11 05:35:15 +0200

    move paginator to edc-dashboard, remove hard coded url

--- edc_search/forms.py
+++ edc_search/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.urls.base import reverse
 
 from crispy_forms.bootstrap import FieldWithButtons, StrictButton
 from crispy_forms.helper import FormHelper
@@ -6,6 +7,9 @@ from crispy_forms.layout import Layout
 
 
 class SearchForm(forms.Form):
+
+    listboard_url_name = 'home_url'
+    button_label = '<i class="fa fa-search fa-sm"></i>'
 
     search_term = forms.CharField(
         label='Search',
@@ -15,11 +19,11 @@ class SearchForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.form_action = None  # set in child class
+        self.helper.form_action = reverse(self.listboard_url_name)
         self.helper.form_id = 'form-search'
         self.helper.form_class = 'form-inline'
         self.helper.form_method = 'post'
         self.helper.form_show_labels = False
         self.helper.html5_required = False
         self.helper.layout = Layout(
-            FieldWithButtons('search_term', StrictButton('<i class="fa fa-search fa-sm"></i>', type='submit')))
+            FieldWithButtons('search_term', StrictButton(self.button_label, type='submit')))
--- edc_search/view_mixins.py
+++ edc_search/view_mixins.py
@@ -1,26 +1,26 @@
 import arrow
 
-from django.core.exceptions import MultipleObjectsReturned
-from django.core.paginator import Paginator, EmptyPage
-from django.db.models import Q
-
-from edc_search.forms import SearchForm
 from arrow.parser import ParserError
 
+from django.core.exceptions import MultipleObjectsReturned
+from django.db.models import Q
+from django.views.generic.edit import FormView
 
-class SearchViewMixin:
+from edc_dashboard.paginator_mixin import PaginatorMixin
+from edc_search.forms import SearchForm
+
+
+class SearchViewMixin(PaginatorMixin, FormView):
 
     form_class = SearchForm
-    list_url = '/'
-    paginate_by = 10
     search_model = None
-    template_name = None
-    url_lookup_parameters = []
-    queryset_ordering = '-created'
+    search_queryset_ordering = '-created'
+    search_model_wrapper_class = None  # any ModelWrapper
 
-    def __init__(self, **kwargs):
-        self.filter_options = {}
-        super().__init__(**kwargs)
+    def get_form_class(self):
+        self.form_class = super().get_form_class()
+        self.form_class.listboard_url_name = self.listboard_url_name
+        return self.form_class
 
     def get_search_options(self, search_term, **kwargs):
         q = Q()
@@ -38,6 +38,10 @@ class SearchViewMixin:
         return q, options
 
     def search_options_for_date(self, search_term, **kwargs):
+        """Returns a Q set and empty options to search on date part of datetime.
+
+        search term assumed to be an arrow object and will be converted
+        to UTC."""
         q = (Q(modified__date=search_term.to('utc').date()) |
              Q(created__date=search_term.to('utc').date()))
         return q, {}
@@ -49,11 +53,27 @@ class SearchViewMixin:
              Q(hostname_modified=search_term))
         return q, {}
 
-    def queryset_wrapper(self, qs):
-        """Wraps either the search queryset or the filtered queryset objects."""
-        return qs
+    def form_valid(self, form):
+        if form.is_valid():
+            search_term = form.cleaned_data['search_term']
+            results = None
+            if search_term:
+                qs = self.search_queryset(search_term, **self.kwargs)
+                if not qs:
+                    form.add_error('search_term', 'No matching records for \'{}\'.'.format(search_term))
+                else:
+                    results = self.paginate(
+                        queryset=qs,
+                        model_wrapper_class=self.search_model_wrapper_class)
+            else:
+                results = self.paginate(
+                    queryset=self.search_model.objects.all().order_by('-created'),
+                    model_wrapper_class=self.search_model_wrapper_class)
+            context = self.get_context_data()
+            context.update(form=form, results=results, search_term=search_term)
+        return self.render_to_response(context)
 
-    def queryset(self, search_term, **kwargs):
+    def search_queryset(self, search_term, **kwargs):
         """Returns a queryset matching the search term passed in through the form, see `form_valid`."""
         q, options = self.get_search_options(search_term, **kwargs)
         try:
@@ -61,69 +81,5 @@ class SearchViewMixin:
         except (self.search_model.DoesNotExist, ValueError):
             qs = None
         except MultipleObjectsReturned:
-            qs = self.search_model.objects.filter(q, **options).order_by(self.queryset_ordering)
+            qs = self.search_model.objects.filter(q, **options).order_by(self.search_queryset_ordering)
         return qs
-
-    def paginate(self, qs):
-        """Paginates a queryset."""
-        paginator = Paginator(qs, self.paginate_by)
-        try:
-            page = paginator.page(self.kwargs.get('page', 1))
-        except EmptyPage:
-            page = paginator.page(paginator.num_pages)
-        page.object_list = self.queryset_wrapper(page.object_list)
-        return page
-
-    def form_valid(self, form):
-        if form.is_valid():
-            search_term = form.cleaned_data['search_term']
-            results = None
-            if search_term:
-                qs = self.queryset(search_term, **self.kwargs)
-                if not qs:
-                    form.add_error('search_term', 'No matching records for \'{}\'.'.format(search_term))
-                else:
-                    results = self.paginate(qs)
-            else:
-                results = self.paginate(self.search_model.objects.all().order_by('-created'))
-            context = self.get_context_data()
-            context.update(form=form, results=results, search_term=search_term)
-        return self.render_to_response(context)
-
-    @property
-    def filtered_results(self):
-        """Returns a queryset filtered by values from the context, see `update_filter_options_from`.
-
-        This is not the "search" queryset"""
-        return self.search_model.objects.filter(**self.filter_options).order_by('-created')
-
-    def update_filter_options_from(self, context):
-        """Intercepts from the context and returns options for the `filtered_results`.
-
-        url_lookup_parameters correspond with parameters defined in urls.py"""
-        lookups = []
-        self.filter_options = {}
-        for attrname in self.url_lookup_parameters:
-            if isinstance(attrname, tuple):
-                lookups.append((attrname[0], attrname[1]))
-            else:
-                lookups.append((attrname, attrname))
-        for attrname, lookup in lookups:
-            if context.get(attrname):
-                self.filter_options.update({lookup: context.get(attrname)})
-                # TODO: ??
-                context['search_term'] = context.get(attrname)
-        return context
-
-    def get_context_data(self, **kwargs):
-        """Updates the context with the paginated filtered results and a few other simple attrs.
-
-        Results are filtered according to the `updated_filter_options_from` the context."""
-        context = super().get_context_data(**kwargs)
-        context = self.update_filter_options_from(context)
-        context.update(
-            show_paginator_control=True if self.filtered_results.count() > self.paginate_by else False,
-            list_url=self.list_url,
-            results=self.paginate(self.filtered_results),
-            result_count_total=self.filtered_results.count())
-        return context
